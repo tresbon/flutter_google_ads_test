f53e4b0ea6fadcd72d24d15441ee5007618b1c49
tresbon <vladi.zubkov@gmail.com>
2021-04-10 23:43:39 +0300

    Added ad init

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  Future<InitializationStatus> _initGoogleMobileAds() {
+    return MobileAds.instance.initialize();
+  }
   @override
   Widget build(BuildContext context) {
+    _initGoogleMobileAds();
     return Container();
   }
 }
-
